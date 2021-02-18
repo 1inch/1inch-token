5bcd43cf0d59faedddd153c9b187c4ebca55a3ec
Mikhail Melnik <by.zumzoom@gmail.com>
2021-02-19 01:09:52 +0300

    add rescueFunds

--- contracts/OneInch.sol
+++ contracts/OneInch.sol
@@ -3,11 +3,14 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./ERC20Permit.sol";
 
 
 contract OneInch is ERC20Permit, ERC20Burnable, Ownable {
+    using SafeERC20 for IERC20;
+
     constructor(address _owner) public ERC20("1INCH Token", "1INCH") EIP712("1INCH Token", "1") {
         transferOwnership(_owner);
     }
@@ -21,5 +24,9 @@ contract OneInch is ERC20Permit, ERC20Burnable, Ownable {
     */
     function getOwner() external view returns (address) {
         return owner();
+    }
+
+    function rescueFunds(IERC20 token, uint256 amount) external onlyOwner {
+        token.safeTransfer(msg.sender, amount);
     }
 }
